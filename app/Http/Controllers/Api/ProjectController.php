7fc3a72afa231a784c4252b8c691b01ebdcbdd0d
Luca Sammarco <luca.sammarco7@gmail.com>
2024-08-20 16:13:37 +0200

    Created Api Project Controller

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+Use App\Models\Type;
+Use App\Models\Technology;
+use App\Model\User;
+
+class ProjectController extends Controller
+{
+    //
+    public function index()
+    {
+        //Ritorna un json con tutti i progetti
+
+        // $projects = Project::all();
+
+        $projects = Project::with("type", "technologies", )->paginate(100);
+
+
+        return
+
+        response()->json(
+
+            [
+            'success' => true,
+            'results' => $projects
+
+            ]
+
+
+        );
+
+    }
+
+    public function show(Project $project)
+    {
+
+    }
+
+}
