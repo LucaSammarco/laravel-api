10e99aaff8dabe8afd69ff531699136709789d59
Luca Sammarco <luca.sammarco7@gmail.com>
2024-08-22 15:57:00 +0200

    incresed number of projects with api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,40 +5,31 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
-Use App\Models\Type;
-Use App\Models\Technology;
-use App\Model\User;
+use App\Models\Type;
+use App\Models\Technology;
+use App\Models\User;
 
 class ProjectController extends Controller
 {
-    //
+    // Metodo per restituire la lista di progetti con relazioni caricate
     public function index()
     {
+        $projects = Project::with('type', 'technologies')->paginate(100);
 
-
-        // $projects = Project::all();
-
-        $projects = Project::with("type", "technologies", )->paginate(100);
-
-
-        return
-
-        response()->json(
-
-            [
+        return response()->json([
             'success' => true,
             'results' => $projects
-
-            ]
-
-
-        );
-
+        ]);
     }
 
+    // Metodo per restituire i dettagli di un singolo progetto
     public function show(Project $project)
     {
+        $project->load('type', 'technologies');
 
+        return response()->json([
+            'success' => true,
+            'results' => $project
+        ]);
     }
-
 }
